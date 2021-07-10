e6295f511bbe4153fce79b20248c7c9a0f909dc6
Stephanie Avila <84059120+phanie-step@users.noreply.github.com>
2021-07-10 16:37:43 -0500

    actualización de rutas

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 router.get('/carrito', productsController.carrito);
 router.get('/producto', productsController.producto);
-router.get('/actualizar_producto', productsController.actualizar_producto);
+router.get('/crearProducto', productsController.crearProducto);
 router.get('/cat_arte', productsController.cat_arte);
 
 module.exports = router;
