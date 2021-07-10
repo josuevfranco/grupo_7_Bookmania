efe24e2266f0acf2f954c1eb415e260f348db356
Stephanie Avila <84059120+phanie-step@users.noreply.github.com>
2021-07-10 14:50:09 -0500

    agregue ruta de productos

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const mainRoutes = require('./routes/mainRoutes');
+const productsRoutes = require('./routes/productsRoutes');
 
 app.use(express.static('./public')); //Carpeta de archivos publicos para no usar request para solicitarlos
 app.set('view engine', 'ejs');
 app.use('/', mainRoutes);
+app.use('/', productsRoutes);
 
 app.listen(PORT, () => {
     console.log('Servidor Corriendo en http://localhost:3000');
