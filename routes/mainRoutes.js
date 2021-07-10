bc1177a0c86ec89a72b6c6bcf11353b0befd5677
Stephanie Avila <84059120+phanie-step@users.noreply.github.com>
2021-07-10 14:49:08 -0500

    actualización de rutas

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -6,10 +6,7 @@ const mainController = require('../controllers/mainController');
 router.get('/', mainController.index);
 router.get('/register', mainController.register);
 router.get('/login', mainController.login);
-router.get('/carrito', mainController.carrito);
-router.get('/producto', mainController.producto);
-router.get('/actualizar_producto', mainController.actualizar_producto);
-router.get('/cat_arte', mainController.cat_arte);
-
 
 module.exports = router;
+
+
