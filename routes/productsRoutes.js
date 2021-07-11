1a48f25fe15dc9aee0b44e0f714cca09b57511b1
Stephanie Avila <84059120+phanie-step@users.noreply.github.com>
2021-07-10 23:58:58 -0500

    rutas main y productos

---
+++ routes/productsRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const productsController = require('../controllers/productsController');
+router.get('/carrito', productsController.carrito);
+router.get('/producto', productsController.producto);
+router.get('/crearProducto', productsController.crearProducto);
+router.get('/cat_arte', productsController.cat_arte);
+
+module.exports = router;
