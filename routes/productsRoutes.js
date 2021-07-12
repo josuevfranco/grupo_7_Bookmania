38604fc926e8093c01e2591ccf6f62264b026134
Pam <pam@MacBook-Air-de-Phanie.local>
2021-07-12 18:17:40 -0500

    campos completos en crearProducto

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -21,7 +21,6 @@ const productsController = require('../controllers/productsController');
 
 //array de validaciones
 const validations = [
-    body('id').notEmpty().withMessage('Tienes que asignarle un id al libro'),
     body('titulo').notEmpty().withMessage('Tienes que escribir el nombre del libro'),
     body('autor').notEmpty().withMessage('Tienes que escrbir el autor del libro'),
     body('year').notEmpty().withMessage('Tienes que escribir el año de publicación'),
@@ -29,6 +28,16 @@ const validations = [
     body('precio')
         .notEmpty().withMessage('Debes de asignar un precio').bail()
         .isNumeric().withMessage('Escribe el precio en formato de número'),
+    
+    body('paginas')
+        .notEmpty().withMessage('Tienes que escribir el total de páginas del libro').bail()
+        .isNumeric().withMessage('Escribe las páginas en formato de número'),
+    
+    body('idioma')
+        .notEmpty().withMessage('Escribe el idioma del libro'),
+    body('rating')
+        .notEmpty().withMessage('Ecribe el raiting del libro').bail()
+        .isNumeric().withMessage('Escribe el raiting en formato de número'),
 
     body('libros').custom((value, {req}) =>{
         let file = req.file;
