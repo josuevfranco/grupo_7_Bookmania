823d0dc305c4c2e34f937f6cbe46cb39d397ebc8
Pam <pam@MacBook-Air-de-Phanie.local>
2021-07-11 21:23:19 -0500

    actualizacion de categorias

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 const mainRoutes = require('./routes/mainRoutes');
 const productsRoutes = require('./routes/productsRoutes');
 
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('./public')); //Carpeta de archivos publicos para no usar request para solicitarlos
 app.set('view engine', 'ejs');
 app.use('/', mainRoutes);
