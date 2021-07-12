7eab724e1a5a6297197e7faf5d34b4081912596d
Pam <pam@MacBook-Air-de-Phanie.local>
2021-07-12 02:33:24 -0500

    instalacion multer

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const { diskStorage } = require('multer');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=> {
+        cb(null, '../public/images/libros');
+    }, 
+    filename: (req, file, cb)=> {
+        const fileName = file.fieldname + '_img' + path.extname(file.filename);
+        cb(null, fileName);
+    }
+})
+const uploadFile = multer({storage});
 
 const productsController = require('../controllers/productsController');
 router.get('/carrito', productsController.carrito);
