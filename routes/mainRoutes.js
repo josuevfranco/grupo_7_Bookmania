c563de723177342f198c5a7abf0b4f1b1d77a665
Pam <pam@MacBook-Air-de-Phanie.local>
2021-07-12 18:56:50 -0500

    validacion en registro terminada

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const multer = require('multer');
+const path = require('path');
 const {body} = require('express-validator');
 
 const mainController = require('../controllers/mainController');
 
+//storage para guardar imagen de libro
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=> {
+        cb(null, './public/images/usuarios');
+    }, 
+    filename: (req, file, cb)=> {
+        const fileName = file.fieldname + '_img' + path.extname(file.originalname);
+        cb(null, fileName);
+    }
+})
+
+const uploadFile = multer({storage});
+
+
+
 //array de validaciones de registro
 const validationsRegister = [
-    body('nameUser').notEmpty().withMessage('Tienes que escribir un nombre'),
-    body('lastName').notEmpty().withMessage('Tienes que escribir un apellido'),
+    body('nameUser').notEmpty().withMessage('Tienes que escribir un nombre (s)'),
+    body('lastName').notEmpty().withMessage('Tienes que escribir tu apellido paterno'),
+    body('lastNameM').notEmpty().withMessage('Tienes que escribir tu apellido materno'),
     body('email')
         .notEmpty().withMessage('Tienes que escribir un email').bail()
         .isEmail().withMessage('Debes escribir un correo válido'),
@@ -16,6 +34,20 @@ const validationsRegister = [
         .notEmpty().withMessage('Debes escribir una contraseña').bail()
         .isLength({min: 6}).withMessage('La contraseña debe ser de mínimo 6 caracteres'),
     body('password2').notEmpty(),
+    
+    body('usuarios').custom((value, {req}) =>{
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.gif'];
+        if(!file){
+            throw new Error('Tienes que subir una imagen de usuario');
+        } else{
+            let fileExtension = path.extname(file.originalname);
+            if(!acceptedExtensions.includes(fileExtension)){
+                throw new Error('Las extensiones de archivos permitidas son ${acceptedExtensions.join(', ')}$')
+            } 
+        }
+        return true;
+    })
 ]
 
 //array de validaciones de login
@@ -35,7 +67,7 @@ router.get('/', mainController.index);
 router.get('/register', mainController.register);
 
 //procesamiento de registro
-router.post('/register', validationsRegister, mainController.processRegister);
+router.post('/register', uploadFile.single('usuarios'), validationsRegister, mainController.processRegister);
 
 //pagina formulario de login
 router.get('/login', mainController.login);
