574ba5e849139602bfbf3c1b67247f2b10c3cb11
Daniela <daniela.lara_h@alumnos.udg.mx>
2021-08-18 21:33:00 -0500

    Diagrama ER

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookies = require('cookie-parser');
 const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
 const methodOverride = require('method-override');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(session({
   secret: "secreto",
